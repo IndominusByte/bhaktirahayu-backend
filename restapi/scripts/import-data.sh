163f0b16267510f0ebd81e06e456f7bbb8b5a843
IndominusByte <nyomanpradipta120@gmail.com>
2021-09-18 23:32:03 +0800

    fix import data db

--- restapi/scripts/import-data.sh
+++ restapi/scripts/import-data.sh
@@ -3,5 +3,5 @@ docker exec -t restapi_postgres_1 psql -U bhaktirahayu -c "COPY users FROM '/hom
 docker exec -t restapi_postgres_1 psql -U bhaktirahayu -c "COPY institutions FROM '/home/institutions_bak.csv' DELIMITER ',' CSV HEADER"
 docker exec -t restapi_postgres_1 psql -U bhaktirahayu -c "COPY guardians FROM '/home/guardians_bak.csv' DELIMITER ',' CSV HEADER"
 docker exec -t restapi_postgres_1 psql -U bhaktirahayu -c "COPY location_services FROM '/home/location_services_bak.csv' DELIMITER ',' CSV HEADER"
-docker exec -t restapi_postgres_1 psql -U bhaktirahayu -c "COPY clients FROM '/home/clients_bak.csv' DELIMITER ',' CSV HEADER"
+docker exec -t restapi_postgres_1 psql -U bhaktirahayu -c "COPY clients (id,nik,name,phone,birth_place,birth_date,gender,address,created_at,updated_at) FROM '/home/clients_bak.csv' DELIMITER ',' CSV HEADER"
 docker exec -t restapi_postgres_1 psql -U bhaktirahayu -c "COPY covid_checkups FROM '/home/covid_checkups_bak.csv' DELIMITER ',' CSV HEADER"
